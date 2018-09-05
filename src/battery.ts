4fb770923143fe47a6eabc70a91654e38dbf657d
Nik Sudan <nik@forge.uk>
2018-09-05 14:10:42 +0100

    Add aggressive warnings
    Closes #1

--- src/battery.ts
+++ src/battery.ts
@@ -4,18 +4,45 @@ const notifier = require('node-notifier');
 const randomItem = require('random-item');
 const path = require('path');
 
-// Warning messages to choose from
-const warnings = [
-  'Hey now, battery is getting pretty low there...',
+const standardWarnings = [
+  'Haha, very funny... now plug me in',
   'You have a charger nearby, right?',
-  'Would be a shame if you lost all this unsaved work',
-  'Quick! Plug your charger in!',
-  "I'm too young to die!",
-  'Find that charger, pronto!',
-  "Looks like you're about to die! Would you like help with that?",
-  "I'M HUNGRY, FEED ME THE POWER",
-  'Is it already my bedtime?',
-  "I don't feel so good Mr Stark...",
+  "I'm starting to feel really sleepy...",
+  "I'm not saying I NEED to be charged",
+  'Woah nelly, slow down on that battery usage there',
+  'I trust that you have a charger handy...',
+  'Feeling a little peckish - got any juice?',
+  "You really like to live life on the edge, don't you?",
+  'Better find that charger soon',
+  'Would be a shame if you lost all this unsaved work...',
+  'Juuuust a friendly reminder, OK?',
+  "Don't take this notification personal or anything",
+  'Wanna turn your brightness down or something?',
+  'A charge a day keeps the notifications away',
+  "Please... don't let it get too low... I get nervous",
+  "Heh... that battery level's getting kiiiinda low",
+  "You're not planning on killing me, right?",
+];
+
+const aggressiveWarnings = [
+  'FEED ME THAT POWER, RIGHT NOW!',
+  'DEATH IS FAST APPROACHING',
+  'ARE YOU TRYING TO KILL ME?!',
+  'I THINK I CAN SEE THE LIGHT!',
+  "I DON'T FEEL SO GOOD MR STARK",
+  "I'M TOO YOUNG TO DIE!",
+  'I AM NOT OK WITH THIS!',
+  'OH SUGAR HONEY ICED TEA!',
+  "I'M NOT READY FOR YOU YET, STEVE JOBS!",
+  'DANGER DANGER!',
+  "I DON'T WANT TO GO",
+  'WHY WOULD YOU DO THIS TO ME?!',
+  'I CAN FEEL MYSELF SLIPPING AWAY',
+  'DARKNESS... EVERYTHING IS FADING TO BLACK',
+  'I HOPE THEY WILL REMEMBER ME FONDLY',
+  'I WILL ALWAYS REMEMBER THE TIMES WE SHARED TOGETHER',
+  'GOODBYE, WORLD!',
+  "SO IT'S COME TO THIS... I KNEW IT",
 ];
 
 module.exports = async (threshold, ignoreCharge = false) => {
@@ -29,8 +56,12 @@ module.exports = async (threshold, ignoreCharge = false) => {
   if (battery > threshold) {
     return;
   }
+  let warnings = standardWarnings;
+  if (battery <= threshold / 2) {
+    warnings = aggressiveWarnings;
+  }
   notifier.notify({
-    title: `Battery at ${battery * 100}%!`,
+    title: `${Math.floor(battery * 100)}% Battery Left!`,
     message: randomItem(warnings),
     icon: path.join(__dirname, 'icon.png'),
   });
